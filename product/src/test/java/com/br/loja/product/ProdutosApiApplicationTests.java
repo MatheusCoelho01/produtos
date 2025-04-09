f32144e9673fe5048374e3f28b1b123b6c2a1c83
Matheus Coelho <matheus.cnasci@gmail.com>
2025-04-08 22:53:08 -0300

    criação de projeto product

---
+++ product/src/test/java/com/br/loja/product/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.loja.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
